599c7fc0f1bd054dd89704271253fe1c90bf3a77
Dmitriy Kanaev <kanaev.dm@mail.ru>
2019-10-20 13:49:13 +0300

    add new Annotations

--- src/main/java/NetworkUtils.java
+++ src/main/java/NetworkUtils.java
@@ -4,7 +4,7 @@ public class NetworkUtils {
     public static void getConnection() {
         // получаем соединение с сервером
         try {
-            TimeUnit.MILLISECONDS.sleep(1500);
+            TimeUnit.MILLISECONDS.sleep(900);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
